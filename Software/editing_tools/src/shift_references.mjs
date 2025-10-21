34391c9f187d972ce7bbe47b67b0d4a8d9339b57
mrpatches123 <190913771+dwc42@users.noreply.github.com>
2025-10-21 15:24:23 -0500

    code to shift refernces

---
+++ Software/editing_tools/src/shift_references.mjs
@@ -0,0 +1,24 @@
+import path from "path";
+import fs from "fs";
+const filePath = path.resolve("../../Reports/Conceptual Design.md");
+const outFileFolder = path.resolve("./out/");
+let file = fs.readFileSync(filePath, "utf-8");
+
+const referenceToShiftFromArray = [...(file.matchAll(/\[(\d+)#\]/g) ?? [])].map(match => Number(match[1]));
+
+if (!referenceToShiftFromArray && !referenceToShiftFromArray.length) throw new Error("No reference to shift found");
+if (referenceToShiftFromArray.length > 1) throw new Error("Multiple references to shift found");
+
+const referenceToShiftFrom = referenceToShiftFromArray[0];
+const { base } = path.parse(filePath) ?? {};
+
+const outPath = path.join(outFileFolder, base);
+console.warn(base, outFileFolder, outPath);
+
+const referencesToShift = [...(file.matchAll(/\[(\d+)\]/g) ?? [])].map(match => Number(match[1])).filter(d => d >= referenceToShiftFrom);
+for (let i = referencesToShift.length - 1; i >= 0; i--) {
+	const referenceToShift = referencesToShift[i];
+	file = file.replaceAll(`[${referenceToShift}]`, `[${referenceToShift + 1}]`);
+}
+file = file.replaceAll(`[${referenceToShiftFrom}#]`, `[${referenceToShiftFrom}]`);
+fs.writeFileSync(outPath, file);
